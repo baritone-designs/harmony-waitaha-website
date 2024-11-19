58b05ee6dcb3904d1fcebd87791ec7bedcfb1845
Joran <joran.lequellec@gmail.com>
2024-11-19 20:03:41 +1300

    feat: Plainsmen mobile site

--- src/app/plainsmen/Header.tsx
+++ src/app/plainsmen/Header.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
-import { m, motion } from 'framer-motion';
-import plainsmenLogo from './icon.svg';
-import './index.css';
+import { m, motion, AnimatePresence } from 'framer-motion';
+import { useClickAway } from 'react-use';
+import { Squash as Hamburger } from 'hamburger-react';
 
 interface HeaderLinkProps {
     url: string;
@@ -14,11 +14,29 @@ interface HeaderLinkProps {
     active: boolean;
 }
 
-/**
- * Component for link in header of organisation layout.
- *
- * Handles underlining based upon current selected page
- */
+const routes = [
+    {
+        title: 'Home',
+        href: '#home',
+        checkActive: 0,
+    },
+    {
+        title: 'About',
+        href: '#about',
+        checkActive: 1,
+    },
+    {
+        title: 'Events',
+        href: '#events',
+        checkActive: 2,
+    },
+    {
+        title: 'Join',
+        href: '#join',
+        checkActive: 3,
+    },
+];
+
 const HeaderLink = ({ children, active, url }: HeaderLinkProps) => (
     <Link href={url} className="relative flex h-8 cursor-pointer items-center gap-2 pb-1 duration-200 hover:opacity-50">
         <span className={clsx(active && 'text-pm-red duration-500', 'text-lg font-semibold duration-500')}>{children}</span>
@@ -32,6 +50,58 @@ const HeaderLink = ({ children, active, url }: HeaderLinkProps) => (
     </Link>
 );
 
+const NavMobile = () => {
+    const [isOpen, setOpen] = useState(false);
+    const ref = useRef(null);
+
+    useClickAway(ref, () => setOpen(false));
+
+    return (
+        <div ref={ref} className="flex justify-end lg:hidden">
+            <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                        className="fixed inset-x-0 top-0 w-1/2 p-5"
+                    >
+                        <ul className="grid gap-2">
+                            {routes.map((route, idx) => {
+                                const { href, title } = route;
+
+                                return (
+                                    <motion.li
+                                        initial={{ x: -500 }}
+                                        animate={{ x: 0 }}
+                                        transition={{
+                                            ease: 'easeOut',
+                                            duration: 0.4,
+                                            delay: 0.1 + idx / 20,
+                                        }}
+                                        key={route.title}
+                                        className="rounded-xl p-0.5"
+                                    >
+                                        <a
+                                            onClick={() => setOpen((prev) => !prev)}
+                                            className="flex w-fit items-center justify-between rounded-full bg-hw-black p-3"
+                                            href={href}
+                                        >
+                                            <span className="flex gap-1 text-white">{title}</span>
+                                        </a>
+                                    </motion.li>
+                                );
+                            })}
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
+    );
+};
+
 export default function PlainsmenHeader() {
     const [active, setActive] = useState(0);
     const headerRef = useRef<HTMLHeadElement>(null);
@@ -44,8 +114,8 @@ export default function PlainsmenHeader() {
             if (!header) return;
 
             elements.forEach((section, i) => {
-                const sectionTop = section.offsetTop;
-                const sectionHeight = section.offsetHeight;
+                const sectionTop = section.offsetTop - 100;
+                const sectionHeight = section.offsetHeight - 100;
 
                 const scrollPosition = window.scrollY + header.offsetHeight;
 
@@ -66,26 +136,33 @@ export default function PlainsmenHeader() {
         <header
             ref={headerRef}
             // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-            className="fixed left-0 z-50 flex h-[10vh] w-screen flex-row items-center justify-between overflow-hidden bg-black bg-opacity-30 px-20"
+            className="fixed z-50 flex w-full justify-center bg-black/0 lg:h-20 lg:bg-black/30"
         >
-            <m.a href="#home" className="w-12" whileHover={{ scale: 1.05 }}>
-                <Image
-                    src={plainsmenLogo}
-                    alt="plainsmen-logo"
-                    height={60}
-                    width={60}
-                />
-            </m.a>
+            <div className="invisible flex h-full w-full max-w-screen-2xl flex-row items-center justify-between px-5 lg:visible lg:px-20 2xl:px-24">
 
-            <nav className="flex flex-row gap-12">
-                <HeaderLink url="#home" active={active === 0}>Home</HeaderLink>
-                <HeaderLink url="#about" active={active === 1}>About</HeaderLink>
-                <HeaderLink url="#events" active={active === 2}>Events</HeaderLink>
-                {/* <HeaderLink url="#media" active={active === 3}>Media</HeaderLink> */}
-                <HeaderLink url="#join" active={active === 4}>Join</HeaderLink>
-            </nav>
+                <m.a href="#home" className="w-12" whileHover={{ opacity: 0.5 }}>
+                    <Image
+                        src="/plainsmen/icon.svg"
+                        alt="plainsmen-logo"
+                        height={60}
+                        width={60}
+                    />
+                </m.a>
 
-            <div className="w-12" />
+                <nav className="hidden flex-row gap-12 lg:flex">
+                    {routes.map((route) => {
+                        const { href, title, checkActive } = route;
+                        return (
+                            <HeaderLink url={href} active={active === checkActive}>{title}</HeaderLink>
+                        );
+                    })}
+                </nav>
+
+                <div className="w-12" />
+            </div>
+            <div className="visible mr-5 mt-5 flex items-center justify-end rounded-full bg-black/50 p-1 lg:invisible">
+                <NavMobile />
+            </div>
         </header>
     );
 }
--- src/app/plainsmen/page.tsx
+++ src/app/plainsmen/page.tsx
@@ -115,91 +115,83 @@ export default async function PlainsmenHome() {
         },
     }));
     return (
-        <main className="bg-hw-black px-20 2xl:px-[10vw]">
+        <main className="bg-hw-black [&>*]:font-poppins">
             <PlainsmenHeader />
 
             <section id="home" className="relative h-screen">
                 <div className="z-10 flex h-full items-center justify-center text-8xl font-medium">
-                    <Image src="/plainsmen-logo.svg" alt="plainsmen-logo" width={500} height={500} className="z-10" />
+                    <Image src="/plainsmen-logo.svg" alt="plainsmen-logo" width={500} height={500} className="z-10 h-32 lg:h-52" />
                 </div>
                 <div className="pointer-events-none absolute -inset-x-20 inset-y-0 2xl:inset-x-[-10vw]">
                     <ScrollImage />
                 </div>
             </section>
 
-            <section id="about" className="my-10 space-y-4">
-                <span className="text-4xl font-semibold text-pm-blue">About Us</span>
-                <div className="grid grid-cols-2 gap-20">
-                    <div>
-                        <p className="z-10">
-                            We are a Barbershop Chorus located in Christchurch, New Zealand that perform a wide range of music blah lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Sed maximus semper lectus fa dfaf asdfasdf f adf asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla rhoncus.
-                            In non mauris lorem. Nullam aliquam massa porta, suscipit
-                        </p>
-                        <div className="z-0 mt-4 grid grid-cols-3 items-center gap-14">
-                            {people.map((person) => (
-                                <TeamProfile key={person.id} {...person} />
+            <div className="flex justify-center">
+                <div className="w-full max-w-screen-2xl px-5 lg:px-20">
+                    <section id="about" className="my-10 space-y-4">
+                        <span className="text-4xl font-semibold text-pm-blue">About Us</span>
+                        <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-20">
+                            <div>
+                                <p className="z-10">
+                                    We are a Barbershop Chorus located in Christchurch, New Zealand that perform a wide range of music blah lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.
+                                    Sed maximus semper lectus fa dfaf asdfasdf f adf asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla rhoncus.
+                                    In non mauris lorem. Nullam aliquam massa porta, suscipit
+                                </p>
+                                <div className="z-0 mt-4 grid grid-cols-2 items-center gap-1 lg:grid-cols-3 lg:gap-14">
+                                    {people.map((person) => (
+                                        <TeamProfile key={person.id} {...person} />
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-between">
+                                <CustomCarousel className="z-10 aspect-video w-full rounded-xl" />
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id="events" className="mb-10 space-y-4">
+                        <span className="text-4xl font-semibold text-pm-blue">Upcoming Events</span>
+                        <div className="grid w-full grid-cols-1 gap-5 lg:grid-cols-3">
+                            {events.map(({ id, ...event }) => (
+                                <EventProfile key={id} {...event} />
                             ))}
                         </div>
-                    </div>
-                    <div className="flex flex-col justify-between">
-                        <CustomCarousel className="z-10 h-96 rounded-xl" />
-                    </div>
-                </div>
-            </section>
+                    </section>
 
-            <section id="events" className="mb-10 space-y-4">
-                <span className="text-4xl font-semibold text-pm-blue">Upcoming Events</span>
-                <div className="grid w-full grid-cols-3 gap-5">
-                    {events.map(({ id, ...event }) => (
-                        <EventProfile key={id} {...event} />
-                    ))}
+                    <section id="join" className="mb-20 space-y-4">
+                        <span className="text-4xl font-semibold text-pm-blue">Sing With Us!</span>
+                        <div className="w-full gap-5 lg:flex lg:flex-row">
+                            <div className="lg:w-2/3">
+                                <p>
+                                    We are a Barbershop Chorus located in Christchurch, New Zealand that perform a wide
+                                    range of music blah lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
+                                    semper lectus fa dfaf asdfasdf f adf asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla
+                                    rhoncus. In non mauris lorem. Nullam aliquam massa porta, suscipit urna a, fringilla sem.
+                                    Quisque sed viverra massa. Nulla sed ipsum erat. Donec maximus eget mauris nec
+                                </p>
+                            </div>
+                            <MapComponent />
+                        </div>
+                    </section>
+                    <section id="footer" className="mb-10 flex flex-row items-end justify-between">
+                        <div>
+                            <span className="text-4xl font-semibold text-pm-blue">Follow Us</span>
+                            <div className="mt-2 flex gap-3 text-white">
+                                <SocialLink icon={FaFacebook} href="https://www.facebook.com/qachorus/" size={25} />
+                            </div>
+                        </div>
+                        <a className="hidden h-full flex-col items-center text-pm-blue duration-200 hover:opacity-50 lg:flex" href="/" target="_blank">
+                            Harmony Waitaha Website ↗
+                        </a>
+                        <div className="flex flex-col items-end">
+                            <Image src="/plainsmen-logo.svg" alt="qa-logo/" width={100} height={100} />
+                            <span>© Plainsmen {new Date().getFullYear()}</span>
+                        </div>
+                    </section>
                 </div>
-            </section>
-
-            <section id="join" className="mb-20 space-y-4">
-                <span className="text-4xl font-semibold text-pm-blue">Sing With Us!</span>
-                <div className="flex w-full flex-row gap-5">
-                    <div className="w-2/3">
-                        <p>
-                            We are a Barbershop Chorus located in Christchurch, New Zealand that perform a wide
-                            range of music blah lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
-                            semper lectus fa dfaf asdfasdf f adf asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla
-                            rhoncus. In non mauris lorem. Nullam aliquam massa porta, suscipit urna a, fringilla sem.
-                            Quisque sed viverra massa. Nulla sed ipsum erat. Donec maximus eget mauris nec
-                            elementum. Suspendisse pulvinar mi nisi, eget venenatis felis tempor vitae. In commodo
-                            risus lacus, et egestas nibh lacinia vitae. We are a Barbershop Chorus located in
-                            Christchurch, New Zealand that perform a wide range of music blah lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit Sed maximus semper lectus fa dfaf asdfasdf f adf
-                            asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla rhoncus. In non mauris lorem.Donec
-                            asdfasdf f adf asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla
-                            rhoncus. In non mauris lorem. Nullam aliquam massa porta, suscipit urna a, fringilla sem.
-                            Quisque sed viverra massa. Nulla sed ipsum erat. Donec maximus eget mauris nec
-                            elementum. Suspendisse pulvinar mi nisi, eget venenatis felis tempor vitae. In commodo
-                            risus lacus, et egestas nibh lacinia vitae. We are a Barbershop Chorus located in
-                            Christchurch, New Zealand that perform a wide range of music blah lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit Sed maximus semper lectus fa dfaf asdfasdf f adf
-                            asdfasdfasdf asdf asdf a dfasdf asdf asdfasdfringilla rhoncus. In non mauris lorem.Donec
-                        </p>
-                    </div>
-                    <MapComponent />
-                </div>
-            </section>
-            <section id="footer" className="mb-10 flex flex-row items-end justify-between">
-                <div>
-                    <span className="text-4xl font-semibold text-pm-blue">Follow Us</span>
-                    <div className="mt-2 flex gap-3 text-white">
-                        <SocialLink icon={FaFacebook} href="https://www.facebook.com/qachorus/" size={25} />
-                    </div>
-                </div>
-                <a className="flex h-full flex-col items-center text-pm-blue duration-200 hover:opacity-50" href="/" target="_blank">
-                    Harmony Waitaha Website ↗
-                </a>
-                <div className="flex flex-col items-end">
-                    <Image src="/plainsmen-logo.svg" alt="qa-logo/" width={100} height={100} />
-                    <span>© Plainsmen {new Date().getFullYear()}</span>
-                </div>
-            </section>
+            </div>
         </main>
     );
 }
