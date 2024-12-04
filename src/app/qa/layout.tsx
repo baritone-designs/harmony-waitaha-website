f0caf4b49c865c0f8eeb4058fdc13ff2ae38e25e
Joran <joran.lequellec@gmail.com>
2024-12-04 22:03:23 +1300

    simplify QA layout.tsx

--- src/app/qa/layout.tsx
+++ src/app/qa/layout.tsx
@@ -1,11 +1,5 @@
-import { Metadata } from 'next';
 import { FC, PropsWithChildren, ReactNode } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-
-export const metadata: Metadata = {
-    title: 'Quantum Acoustics',
-    description: 'Youth Barbershop Harmony chorus in Christchurch New Zealand',
-};
 
 const QaLayout: FC<PropsWithChildren<{
     person: ReactNode;
