8460af61d61f70c388ef95a4c2ff348fe640b825
Joran <joran.lequellec@gmail.com>
2025-02-12 12:24:05 +1300

    feat: quartets in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,38 @@
+import { prisma } from '@/common/prisma';
 import type { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const quartets = await prisma.quartet.findMany({
+        select: {
+            id: true,
+        },
+    });
+    const quartetsSitemap = quartets.map((quartet) => ({
+        url: `https://www.harmonywaitaha.co.nz/?quartet=${quartet.id}`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly' as const,
+        priority: 0.5,
+    }));
+
     return [
         {
             url: 'https://www.harmonywaitaha.co.nz/',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
+            changeFrequency: 'weekly',
             priority: 1,
         },
         {
             url: 'https://www.harmonywaitaha.co.nz/qa',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.5,
+            changeFrequency: 'weekly',
+            priority: 1,
         },
         {
             url: 'https://www.harmonywaitaha.co.nz/plainsmen',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.5,
+            changeFrequency: 'weekly',
+            priority: 1,
         },
+        ...quartetsSitemap,
     ];
 }
