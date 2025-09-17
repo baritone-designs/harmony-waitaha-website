6473d51ddcfb8c4b8f65c35cf1d49f7568c9eba4
Alex Cutforth <alex@densem.net>
2025-09-17 16:53:49 +1200

    persist state

--- src/components/ConventionPopup.tsx
+++ src/components/ConventionPopup.tsx
@@ -3,22 +3,26 @@
 import clsx from 'clsx';
 import { m, AnimatePresence } from 'framer-motion';
 import { Close } from '@mui/icons-material';
-import { useState } from 'react';
+import { useCookie } from 'react-use';
 import ModalBackdrop from './ModalBackdrop';
 
 export default function ConventionPopup() {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useCookie('convention-popup');
 
-    const show = open && (new Date() < new Date('2025-09-22'));
+    if (open === null) {
+        setOpen('true');
+    }
+
+    const show = (open === 'true') && (new Date() < new Date('2025-09-22'));
 
     return (
         <>
-            <button className="fixed left-0 top-1/2 z-10 -translate-y-1/2 rounded-r-3xl bg-hw-white p-5 shadow-lg duration-200 hover:px-7" type="button" onClick={() => setOpen(true)}>
+            <button className="fixed left-0 top-1/2 z-10 -translate-y-1/2 rounded-r-3xl bg-hw-white p-5 shadow-lg duration-200 hover:px-7" type="button" onClick={() => setOpen('true')}>
                 <span className="text-3xl" style={{ writingMode: 'vertical-rl' }}>Convention</span>
             </button>
             <AnimatePresence>
                 {show && (
-                    <ModalBackdrop onClose={() => setOpen(false)}>
+                    <ModalBackdrop onClose={() => setOpen('false')}>
                         <m.div
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
@@ -35,7 +39,7 @@ export default function ConventionPopup() {
                             <span className="text-4xl">BHNZ 2025 Convention</span>
                             <span className="text-3xl">Hosted by Harmony Waitaha</span>
                             <span>19 - 21 September 2025 | James Hay Theatre, 86 Kilmore Street, Christchurch</span>
-                            <button onClick={() => setOpen(false)} type="button">
+                            <button onClick={() => setOpen('false')} type="button">
                                 <Close className="absolute right-5 top-5 text-hw-black duration-200 hover:opacity-75" />
                             </button>
                             <div className="flex flex-row gap-5 text-xl text-hw-black underline">
